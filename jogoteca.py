31eb96aa877e3320cc8bad86ac63aa28360c8629
ramonzeraa <ramoncandido64@gmail.com>
2024-10-04 00:42:00 -0300

    utilizado a url_for para boas praticas utilizando a função que instancia a pagina

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect , session, flash
+from flask import Flask, render_template, request, redirect , session, flash, url_for
 
 app = Flask(__name__)
 app.secret_key = 'fuK1duQhhi1Q4a0'
@@ -20,10 +20,10 @@ def index():
     return render_template('lista.html', titulo = 'Jogos', jogos = lista) #indica O PARAMETRO criado no html por boas praticas
 
 
-@app.route('/novo')
+@app.route('/novo ')
 def novo():
     if 'usuario_logado' not in session or session ['usuario_logado'] == None:
-        return redirect('/login?proxima=novo')#query string
+        return redirect(url_for('login', proxima=url_for('novo')))#query string
     return render_template('novo.html', titulo = 'Novo Jogo')
 
 
@@ -34,12 +34,12 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome ,categoria, console)
     lista.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))#função que instancia a pagina
 
 
 @app.route('/login')
 def login():
-    proxima = request.args.get('proxima')
+    proxima = request.args.get('proxima')#recuperando dados passados pela query string
     return render_template('login.html' , proxima = proxima)
 
 
@@ -50,17 +50,17 @@ def autenticar():
         session['usuario_logado'] = request.form['usuário']
         flash(session['usuario_logado'] + ' logado com sucesso')
         proxima_pagina = request.form['proxima']
-        return redirect(f'/{proxima_pagina}')
+        return redirect(proxima_pagina)
     else:
         flash('Usuário não logado')
-        return redirect('/login')
+        return redirect(url_for('login'))
 
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso')
-    return redirect('/')
+    return redirect(url_for('index'))
 
 app.run(debug = True)  
     
