f9c53ca3cf80aeb01011642af9628be3487917fd
ramonzeraa <ramoncandido64@gmail.com>
2024-10-04 01:26:00 -0300

    Error 400 Bad Request: The browser (or proxy) sent a request that this server could not understand., sem resolução até então

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect , session, flash, url_for
+from flask import Flask, render_template, request, redirect , session, flash
 
 app = Flask(__name__)
 app.secret_key = 'fuK1duQhhi1Q4a0'
@@ -15,15 +15,29 @@ jogo3 = Jogo('Valorant','Tiro','PC')
 jogo4 = Jogo('Doom','RPG',  'PC')
 lista = [jogo1,jogo2,jogo3,jogo4]
 
+class Usuario:
+    def __init__(self, nome, nickname, senha):
+        self.nome = nome
+        self.nickname = nickname
+        self.senha = senha
+        
+usuario1 = Usuario("Ramon Candido" , "Ramones" , "7532draivp")
+usuario2 = Usuario("Murilo huff" , "Murinelas" , "1234567")
+usuario3 = Usuario("Leticia neves" , "Leticines" ,"abcdef")
+
+usuarios = { usuario1.nickname : usuario1,
+            usuario2.nickname : usuario2, 
+            usuario3.nickname : usuario3 }
+
 @app.route('/')
 def index():
     return render_template('lista.html', titulo = 'Jogos', jogos = lista) #indica O PARAMETRO criado no html por boas praticas
 
 
-@app.route('/novo ')
+@app.route('/novo')
 def novo():
     if 'usuario_logado' not in session or session ['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('novo')))#query string
+        return redirect('/login?proxima=novo')#query string
     return render_template('novo.html', titulo = 'Novo Jogo')
 
 
@@ -34,33 +48,35 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome ,categoria, console)
     lista.append(jogo)
-    return redirect(url_for('index'))#função que instancia a pagina
+    return redirect('/')
 
 
 @app.route('/login')
 def login():
-    proxima = request.args.get('proxima')#recuperando dados passados pela query string
+    proxima = request.args.get('proxima')
     return render_template('login.html' , proxima = proxima)
 
 
 
 @app.route('/autenticar' , methods = ['POST',])
 def autenticar():
-    if 'teste' == request.form['Senha']:
-        session['usuario_logado'] = request.form['usuário']
-        flash(session['usuario_logado'] + ' logado com sucesso')
-        proxima_pagina = request.form['proxima']
-        return redirect(proxima_pagina)
+    if  request.form['usuário'] in usuarios:
+        usuario = usuarios[request.form['usuário']]
+        if request.form["senha"] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname) + ' logado com sucesso'
+            proxima_pagina =  request.form['proxima']
+            return redirect('/')
     else:
         flash('Usuário não logado')
-        return redirect(url_for('login'))
+        return redirect('/login')
 
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso')
-    return redirect(url_for('index'))
+    return redirect('/')
 
 app.run(debug = True)  
     
