2343b0f4fd43321ce294585110417999d489869d
ramonzeraa <ramoncandido64@gmail.com>
2024-10-07 16:12:35 -0300

    Inserindo as tabelas jogos e usuarios feitas anteriormente no banco de dados recem criado e mostrando no terminal

--- receber_banco.py
+++ receber_banco.py
@@ -25,17 +25,17 @@ cursor.execute("CREATE DATABASE Jogoteca;")
 cursor.execute("USE Jogoteca;")
 
 TABLES = {}
-# TABLES['Jogos'] = ('''
-#      CREATE TABLE jogos (
-#          id int(11) not null auto_increment,
-#          nome varchar(50) not null,
-#          categoria varchar(40) not null,
-#          console varchar(20) not null,
-#          PRIMARY KEY (id)
-#      )''')
+TABLES['Jogos'] = ('''
+     CREATE TABLE Jogos (
+         id int(11) not null auto_increment,
+         nome varchar(50) not null,
+         categoria varchar(40) not null,
+         console varchar(20) not null,
+         PRIMARY KEY (id)
+     )''')
 
 TABLES['Usuarios'] = ('''
-      CREATE TABLE  usuarios (
+      CREATE TABLE  Usuarios (
           nome varchar(20) not null,
           nickname varchar(20) not null,
           senha varchar(100) not null,
@@ -55,3 +55,31 @@ for tabela_nome in TABLES:
     else:
         print('OK')
         
+#inserindo usuários
+usuario_sql = 'INSERT INTO Usuarios (nome,nickname,senha) VALUES (%s, %s, %s)'
+Usuarios = [
+    ("Ramon Candido" , "Ramones" , "7532draivp"),
+    ("Murilo huff" , "Murinelas" , "1234567"),
+    ("Leticia neves" , "Leticines" ,"abcdef")
+]        
+cursor.executemany(usuario_sql,Usuarios)
+
+cursor.execute('select * from Usuarios')
+print('------------ Usuários: ---------------')
+for user in cursor.fetchall():
+    print(user[1])
+    
+#inserindo jogos
+jogos_sql = 'INSERT INTO Jogos (nome, categoria, console) VALUES (%s , %s, %s)'
+jogos = [
+    ('Tetris', 'Puzzle', 'Atari'),
+    ('Lol', 'MMO','PC'),
+    ('Valorant','Tiro','PC'),
+    ('Doom','RPG',  'PC')
+]
+
+cursor.executemany(jogos_sql , jogos)
+cursor.execute('select * from Jogos')
+print('------------ Jogos: ---------------')
+for jogo in cursor.fetchall():
+    print(jogo[1])
