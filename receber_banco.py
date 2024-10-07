913468acc923dc672abe3c2c37829c611a75ba1e
ramonzeraa <ramoncandido64@gmail.com>
2024-10-07 15:40:21 -0300

    Criado o cursor e criado o DATABASE Jogotece, pronto para uso

--- receber_banco.py
+++ receber_banco.py
@@ -1,8 +1,27 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-conexao = mysql.connector.connect(
-    host = '127.0.0.1',
-    user = 'root',
-    password = '7532draivp',
-)
+
+print("Testando conexão...")
+
+try:
+    conexao = mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        password = '7532draivp',
+    )
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Erro no usuario ou senha")
+    else:
+        print('conexão bem sucedida')
+        
+cursor = conexao.cursor()
+
+cursor.execute("DROP DATABASE IF EXISTS Jogoteca;")
+
+cursor.execute("CREATE DATABASE Jogoteca;")
+
+cursor.execute("USE Jogoteca;")
+
+        
