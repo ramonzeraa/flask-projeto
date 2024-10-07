9c53073d2fec90de084f20155cdf02393cc88096
ramonzeraa <ramoncandido64@gmail.com>
2024-10-07 15:51:29 -0300

    Criado a tabela Usuarios no db Jogoteca

--- receber_banco.py
+++ receber_banco.py
@@ -25,15 +25,39 @@ cursor.execute("CREATE DATABASE Jogoteca;")
 cursor.execute("USE Jogoteca;")
 
 TABLES = {}
-TABLES['Jogos'] = ('''
-     CREATE TABLE jogos (
-         id int(11) not null auto_increment,
-         nome varchar(50) not null,
-         categoria varchar(40) not null,
-         console varchar(20) not null,
-         PRIMARY KEY (id)
-     )''')
+# TABLES['Jogos'] = ('''
+#      CREATE TABLE jogos (
+#          id int(11) not null auto_increment,
+#          nome varchar(50) not null,
+#          categoria varchar(40) not null,
+#          console varchar(20) not null,
+#          PRIMARY KEY (id)
+#      )''')
+
+TABLES['Usuarios'] = ('''
+      CREATE TABLE  usuarios (
+          nome varchar(20) not null,
+          nickname varchar(20) not null,
+          senha varchar(100) not null,
+          PRIMARY KEY (nickname)
+          )''')
 
 
 
-        
+
+
+
+
+
+
+
+# for tabela_nome in TABLES:
+#     tabela_sql = TABLES[tabela_nome]
+#     try:
+#         print(f'Criando tabela {tabela_nome}' , end = '')
+#         cursor.execute(tabela_sql)
+#     except mysql.connector.Error as err:
+#         if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+#             print('Já existe')
+#         else:
+#             print(err)
